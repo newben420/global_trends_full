2d3392323cd60ab4193b19dfff348407d815efef
newben420 <wraith@wraith.wraith>
2025-08-15 22:59:20 +0100

    Added supabase backup feature, local translate loader, and updated banner

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -11,7 +11,6 @@ import { LOCALES } from './locales';
 import { SocketIoConfig, provideSocketIo } from 'ngx-socket-io';
 import { Store } from './services/store';
 import { isPlatformServer } from '@angular/common';
-import { UniversalTranslateLoader } from './universal-translate-loader';
 import { JsonFileLoader } from './custom-translate-loader';
 
 const config: SocketIoConfig = { url: '/', options: {} };
@@ -24,19 +23,6 @@ function initTranslations() {
       translate.use(r);
     }
   })
-}
-
-function provideUniversalTranslateLoader() {
-  const platformId = inject(PLATFORM_ID);
-  const http = inject(HttpClient);
-
-  if (isPlatformServer(platformId)) {
-    return new UniversalTranslateLoader(http);
-  } else {
-    // fallback: the default Http loader
-    const { TranslateHttpLoader } = require('@ngx-translate/http-loader');
-    return new TranslateHttpLoader(http, '/i18n/', '.json');
-  }
 }
 
 export const appConfig: ApplicationConfig = {
